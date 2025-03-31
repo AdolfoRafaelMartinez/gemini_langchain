e33641db63a66b454a22984fbfc4f7a2445deb72
adrama <adrama@utexas.edu>
2025-03-31 16:08:33 -0500

    modified go.sh

--- go.sh
+++ go.sh
@@ -1,2 +1,2 @@
 # /bin/sh
-docker run -it --rm -v "$PWD":/app -w /app -p 8000:8000 --entrypoint=/bin/bash adrama/python:v1
+docker run -it --rm -v "$PWD":/app -w /app -p 8000:8000 --entrypoint=/bin/bash python:3.11
---
+++ readme.txt
@@ -0,0 +1,3 @@
+pip install dotenv
+pip install genai
+pip install google-generativeai==0.3.1
