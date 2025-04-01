83ff01f2ead706c1db5a38a6a0705443a0df190b
adrama <adrama@utexas.edu>
2025-03-31 19:59:29 -0500

    smoke works

--- smoke_test.py
+++ smoke_test.py
@@ -1,9 +1,11 @@
+import os
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-client = genai.Client(api_key="GEMINI_API_KEY")
+client = genai.Client(api_key=gemini_api_key)
 
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Explain how AI works in a few words"
